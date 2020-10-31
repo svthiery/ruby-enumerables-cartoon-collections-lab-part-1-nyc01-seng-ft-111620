45a5e3f72d372fd64bde05c3c57e1b6fb28d926e
Sarah Kain Thiery <sarahvkain@gmail.com>
2020-10-31 22:26:00 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,7 +5,7 @@ def greet_characters(array)
 end
 
 def list_dwarves(array)
-  array.each_with_index do |element|
-    puts "#{"
+  array.each_with_index do |element, index|
+    puts "#{index + 1}. element"
   end
 end
